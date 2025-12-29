877568c927fdb217b1bcad8a62024affe356f882
Tsumida <tsumida@163.com>
2025-12-29 16:36:51 +0800

    init rocksdbStore

--- crates/rlr/src/storage.rs
+++ crates/rlr/src/storage.rs
@@ -1,117 +1,314 @@
-// use crate::state_machine::StateMachine;
-// use crate::wal::WriteAheadLog;
-// use bytes::Bytes;
-use getset::Getters;
-use serde::{Deserialize, Serialize};
-use std::collections::BTreeMap;
+//! Copy from https://github.com/databendlabs/openraft/blob/main/examples/rocksstore/src/log_store.rs
+
+use std::error::Error;
 use std::fmt::Debug;
-use std::io::{self, Cursor};
+use std::io;
+use std::marker::PhantomData;
 use std::ops::RangeBounds;
 use std::sync::Arc;
-use std::sync::atomic::{AtomicU64, Ordering};
 
-use crate::types::{AppEntry, AppNodeId, AppResponse, AppTypeConfig};
-use futures::{Stream, TryStreamExt};
+use byteorder::BigEndian;
+use byteorder::ReadBytesExt;
+use byteorder::WriteBytesExt;
+use meta::StoreMeta;
+use openraft::LogState;
+use openraft::OptionalSend;
+use openraft::RaftLogReader;
+use openraft::RaftTypeConfig;
+use openraft::TokioRuntime;
+use openraft::entry::RaftEntry;
+use openraft::storage::IOFlushed;
+use openraft::storage::RaftLogStorage;
+use openraft::type_config::alias::EntryOf;
+use openraft::type_config::alias::LogIdOf;
+use openraft::type_config::alias::VoteOf;
+use rocksdb::ColumnFamily;
+use rocksdb::DB;
+use rocksdb::Direction;
+use serde_json;
+use tokio::task::spawn_blocking;
 
-use openraft::{
-    Entry,
-    EntryPayload,
-    LogId,
-    OptionalSend,
-    RaftLogReader,
-    RaftTypeConfig,
-    StorageError,
-    StoredMembership,
-    // alias::SnapshotDataOf,
-    storage::{
-        EntryResponder, IOFlushed, LogState, RaftLogStorage, RaftSnapshotBuilder, RaftStateMachine,
-        Snapshot, SnapshotMeta,
-    },
-};
+//
+// CF:
+//    1. meta: for raft metadata
+//    2. logs: for raft log entries
+//
+const CF_META: &'static str = "meta";
+const CF_LOGS: &'static str = "logs";
 
-#[derive(Debug, Getters)]
-struct RlrStore<S: Send + Sync + 'static> {
-    /// The Raft log.
-    // pub log: sled::Tree, //RwLock<BTreeMap<u64, Entry<StorageRaftTypeConfig>>>,
-    // vote: sled::Tree,
-
-    /// The Raft state machine.
-    #[getset(get = "pub")]
-    state_machine: tokio::sync::RwLock<S>,
-
-    #[getset(get = "pub")]
-    node_id: AppNodeId,
-
-    /// The current granted vote.
-    current_snapshot: tokio::sync::RwLock<Option<RlrSnapshot>>,
-    last_purged_log_id: Option<LogId<AppNodeId>>,
-    config: AppTypeConfig,
+#[derive(Debug, Clone)]
+pub struct RocksLogStore<C>
+where
+    C: RaftTypeConfig,
+{
+    db: Arc<DB>,
+    _p: PhantomData<C>,
 }
 
-impl<S: Send + Sync + 'static> RaftLogReader<AppTypeConfig> for RlrStore<S> {
-    async fn try_get_log_entries<RB: RangeBounds<u64> + Clone + Debug + Send>(
+impl<C> RocksLogStore<C>
+where
+    C: RaftTypeConfig,
+{
+    pub fn new(db: Arc<DB>) -> Self {
+        db.cf_handle(CF_META)
+            .expect("column family `meta` not found");
+        db.cf_handle(CF_LOGS)
+            .expect("column family `logs` not found");
+
+        Self {
+            db,
+            _p: Default::default(),
+        }
+    }
+
+    fn cf_meta(&self) -> &ColumnFamily {
+        self.db.cf_handle(CF_META).unwrap()
+    }
+
+    fn cf_logs(&self) -> &ColumnFamily {
+        self.db.cf_handle(CF_LOGS).unwrap()
+    }
+
+    /// Get a store metadata.
+    ///
+    /// It returns `None` if the store does not have such a metadata stored.
+    fn get_meta<M: StoreMeta<C>>(&self) -> Result<Option<M::Value>, io::Error> {
+        let bytes = self
+            .db
+            .get_cf(self.cf_meta(), M::KEY)
+            .map_err(|e| io::Error::other(e.to_string()))?;
+
+        let Some(bytes) = bytes else {
+            return Ok(None);
+        };
+
+        let t = serde_json::from_slice(&bytes)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+        Ok(Some(t))
+    }
+
+    /// Save a store metadata.
+    fn put_meta<M: StoreMeta<C>>(&self, value: &M::Value) -> Result<(), io::Error> {
+        let json_value =
+            serde_json::to_vec(value).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+        self.db
+            .put_cf(self.cf_meta(), M::KEY, json_value)
+            .map_err(|e| io::Error::other(e.to_string()))?;
+
+        Ok(())
+    }
+}
+
+impl<C> RaftLogReader<C> for RocksLogStore<C>
+where
+    C: RaftTypeConfig,
+{
+    async fn try_get_log_entries<RB: RangeBounds<u64> + Clone + Debug + OptionalSend>(
         &mut self,
         range: RB,
-    ) -> Result<Vec<Entry<AppTypeConfig>>, io::Error> {
-        todo!()
+    ) -> Result<Vec<C::Entry>, io::Error> {
+        let start = match range.start_bound() {
+            std::ops::Bound::Included(x) => id_to_bin(*x),
+            std::ops::Bound::Excluded(x) => id_to_bin(*x + 1),
+            std::ops::Bound::Unbounded => id_to_bin(0),
+        };
+
+        let mut res = Vec::new();
+
+        let it = self.db.iterator_cf(
+            self.cf_logs(),
+            rocksdb::IteratorMode::From(&start, Direction::Forward),
+        );
+        for item_res in it {
+            let (id, val) = item_res.map_err(read_logs_err)?;
+
+            let id = bin_to_id(&id);
+            if !range.contains(&id) {
+                break;
+            }
+
+            let entry: EntryOf<C> = serde_json::from_slice(&val).map_err(read_logs_err)?;
+
+            assert_eq!(id, entry.index());
+
+            res.push(entry);
+        }
+        Ok(res)
     }
 
-    async fn read_vote(&mut self) -> Result<Option<openraft::Vote<AppTypeConfig>>, io::Error> {
-        todo!()
+    async fn read_vote(&mut self) -> Result<Option<VoteOf<C>>, io::Error> {
+        self.get_meta::<meta::Vote>()
     }
 }
 
-impl<S: Send + Sync + 'static> RaftLogStorage<AppTypeConfig> for RlrStore<S> {
+impl<C> RaftLogStorage<C> for RocksLogStore<C>
+where
+    C: RaftTypeConfig<AsyncRuntime = TokioRuntime>,
+{
     type LogReader = Self;
 
-    async fn get_log_state(&mut self) -> Result<LogState<AppTypeConfig>, io::Error> {
-        todo!()
-    }
+    async fn get_log_state(&mut self) -> Result<LogState<C>, io::Error> {
+        let last = self
+            .db
+            .iterator_cf(self.cf_logs(), rocksdb::IteratorMode::End)
+            .next();
 
-    async fn save_committed(
-        &mut self,
-        committed: Option<LogId<AppTypeConfig>>,
-    ) -> Result<(), io::Error> {
-        todo!()
-    }
+        let last_log_id = match last {
+            None => None,
+            Some(res) => {
+                let (_log_index, entry_bytes) = res.map_err(read_logs_err)?;
+                let ent =
+                    serde_json::from_slice::<EntryOf<C>>(&entry_bytes).map_err(read_logs_err)?;
+                Some(ent.log_id())
+            }
+        };
 
-    async fn read_committed(&mut self) -> Result<Option<LogId<AppTypeConfig>>, io::Error> {
-        todo!()
-    }
+        let last_purged_log_id = self.get_meta::<meta::LastPurged>()?;
 
-    async fn save_vote(&mut self, vote: &openraft::Vote<AppTypeConfig>) -> Result<(), io::Error> {
-        todo!()
-    }
+        let last_log_id = match last_log_id {
+            None => last_purged_log_id.clone(),
+            Some(x) => Some(x),
+        };
 
-    async fn append<I>(
-        &mut self,
-        entries: I,
-        callback: IOFlushed<AppTypeConfig>,
-    ) -> Result<(), io::Error>
-    where
-        I: IntoIterator<Item = Entry<AppTypeConfig>> + OptionalSend,
-        I::IntoIter: OptionalSend,
-    {
-        todo!()
-    }
-
-    async fn truncate(&mut self, log_id: LogId<AppTypeConfig>) -> Result<(), io::Error> {
-        todo!()
-    }
-
-    async fn purge(&mut self, log_id: LogId<AppTypeConfig>) -> Result<(), io::Error> {
-        todo!()
+        Ok(LogState {
+            last_purged_log_id,
+            last_log_id,
+        })
     }
 
     async fn get_log_reader(&mut self) -> Self::LogReader {
-        todo!()
+        self.clone()
+    }
+
+    async fn save_vote(&mut self, vote: &VoteOf<C>) -> Result<(), io::Error> {
+        self.put_meta::<meta::Vote>(vote)?;
+
+        // Vote must be persisted to disk before returning.
+        let db = self.db.clone();
+        spawn_blocking(move || db.flush_wal(true))
+            .await
+            .map_err(|e| io::Error::other(e.to_string()))?
+            .map_err(|e| io::Error::other(e.to_string()))?;
+
+        Ok(())
+    }
+
+    async fn append<I>(&mut self, entries: I, callback: IOFlushed<C>) -> Result<(), io::Error>
+    where
+        I: IntoIterator<Item = EntryOf<C>> + Send,
+    {
+        for entry in entries {
+            let id = id_to_bin(entry.index());
+            self.db
+                .put_cf(
+                    self.cf_logs(),
+                    id,
+                    serde_json::to_vec(&entry)
+                        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?,
+                )
+                .map_err(|e| io::Error::other(e.to_string()))?;
+        }
+
+        // spown_block()用于同步IO, 避免阻塞调度器
+        let db = self.db.clone();
+        let handle = spawn_blocking(move || {
+            let res = db.flush_wal(true).map_err(io::Error::other);
+            // io_completed有什么作用
+            callback.io_completed(res);
+        });
+        drop(handle);
+
+        // Return now, and the callback will be invoked later when IO is done.
+        Ok(())
+    }
+
+    // 删除[log_id, +oo)的日志
+    async fn truncate(&mut self, log_id: LogIdOf<C>) -> Result<(), io::Error> {
+        tracing::debug!("truncate: [{:?}, +oo)", log_id);
+
+        let from = id_to_bin(log_id.index());
+        let to = id_to_bin(u64::MAX);
+        self.db
+            .delete_range_cf(self.cf_logs(), &from, &to)
+            .map_err(|e| io::Error::other(e.to_string()))?;
+
+        // Truncating does not need to be persisted.
+        Ok(())
+    }
+
+    async fn purge(&mut self, log_id: LogIdOf<C>) -> Result<(), io::Error> {
+        tracing::debug!("delete_log: [0, {:?}]", log_id);
+
+        // Write the last-purged log id before purging the logs.
+        // The logs at and before last-purged log id will be ignored by openraft.
+        // Therefore, there is no need to do it in a transaction.
+        self.put_meta::<meta::LastPurged>(&log_id)?;
+
+        let from = id_to_bin(0);
+        let to = id_to_bin(log_id.index() + 1);
+        self.db
+            .delete_range_cf(self.cf_logs(), &from, &to)
+            .map_err(|e| io::Error::other(e.to_string()))?;
+
+        // Purging does not need to be persistent.
+        Ok(())
     }
 }
 
-#[derive(Debug)]
-struct RlrSnapshot {
-    pub meta: SnapshotMeta<AppNodeId>,
+/// Metadata of a raft-store.
+///
+/// In raft, except logs and state machine, the store also has to store several piece of metadata.
+/// This sub mod defines the key-value pairs of these metadata.
+mod meta {
+    use openraft::RaftTypeConfig;
+    use openraft::type_config::alias::LogIdOf;
+    use openraft::type_config::alias::VoteOf;
 
-    /// The data of the state machine at the time of this snapshot.
-    pub data: Vec<u8>,
+    /// Defines metadata key and value
+    pub(crate) trait StoreMeta<C>
+    where
+        C: RaftTypeConfig,
+    {
+        /// The key used to store in rocksdb
+        const KEY: &'static str;
+
+        /// The type of the value to store
+        type Value: serde::Serialize + serde::de::DeserializeOwned;
+    }
+
+    pub(crate) struct LastPurged {}
+    pub(crate) struct Vote {}
+
+    impl<C> StoreMeta<C> for LastPurged
+    where
+        C: RaftTypeConfig,
+    {
+        const KEY: &'static str = "last_purged_log_id";
+        type Value = LogIdOf<C>;
+    }
+    impl<C> StoreMeta<C> for Vote
+    where
+        C: RaftTypeConfig,
+    {
+        const KEY: &'static str = "vote";
+        type Value = VoteOf<C>;
+    }
+}
+
+/// converts an id to a byte vector for storing in the database.
+/// Note that we're using big endian encoding to ensure correct sorting of keys
+fn id_to_bin(id: u64) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(8);
+    buf.write_u64::<BigEndian>(id).unwrap();
+    buf
+}
+
+fn bin_to_id(buf: &[u8]) -> u64 {
+    (&buf[0..8]).read_u64::<BigEndian>().unwrap()
+}
+
+fn read_logs_err(e: impl Error + 'static) -> io::Error {
+    io::Error::other(e.to_string())
 }
